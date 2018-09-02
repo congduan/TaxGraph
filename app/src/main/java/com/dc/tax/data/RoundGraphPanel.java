44dc53e83e302d68070844f00d02886b3f65f559
congduan <congduan@tencent.com>
2018-09-02 12:52:51 +0800

    update

--- app/src/main/java/com/dc/tax/data/RoundGraphPanel.java
+++ app/src/main/java/com/dc/tax/data/RoundGraphPanel.java
@@ -3,9 +3,13 @@ package com.dc.tax.data;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 
+/**
+ * 饼状图
+ */
 public class RoundGraphPanel implements Callback {
 
     private static final String TAG = RoundGraphPanel.class.getSimpleName();
@@ -32,13 +36,13 @@ public class RoundGraphPanel implements Callback {
 
     public void init() {
         strokePaint = new Paint();
-        strokePaint.setColor(Color.RED);
+        strokePaint.setColor(Color.BLUE);
         strokePaint.setAntiAlias(true);
         strokePaint.setStrokeWidth(3);
         strokePaint.setStyle(Paint.Style.STROKE);
 
         fillPaint = new Paint();
-        fillPaint.setColor(Color.WHITE);
+        fillPaint.setColor(Color.GREEN);
         fillPaint.setAntiAlias(true);
         fillPaint.setStrokeWidth(3);
         fillPaint.setStyle(Paint.Style.FILL);
@@ -82,7 +86,27 @@ public class RoundGraphPanel implements Callback {
         strokePaint.setPathEffect(null);
         canvas.drawRect(0, 0, width, height, strokePaint);
 
+        // draw arc
+        int min = Math.min(width, height);
+        float padding = 50.0f;
+        float radius = (min - padding) / 2;
+        float centerX = width / 2.0f;
+        float centerY = height / 2.0f;
 
+        // draw circle
+        strokePaint.setColor(Color.BLACK);
+        canvas.drawCircle(centerX, centerY, radius, strokePaint);
+
+        RectF rectF = new RectF(
+                centerX - radius,
+                centerY - radius,
+                centerX + radius,
+                centerY + radius);
+        canvas.drawArc(rectF, 0, 90, true, fillPaint);
+
+        // draw arc stroke
+        strokePaint.setColor(Color.BLUE);
+        canvas.drawArc(rectF, 0, 90, true, strokePaint);
 
         mSurfaceHolder.unlockCanvasAndPost(canvas);
     }
